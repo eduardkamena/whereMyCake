baccab0dfe3b2afa5349fc7eab88baa29b0b6069
Ed <eduard@kamenskikh.me>
2025-03-17 23:06:42 +0300

    Initial commit

---
+++ src/main/java/ru/systems1221/whereMyCake/WhereMyCakeApplication.java
@@ -0,0 +1,13 @@
+package ru.systems1221.whereMyCake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereMyCakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhereMyCakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/systems1221/whereMyCake/WhereMyCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.systems1221.whereMyCake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereMyCakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
